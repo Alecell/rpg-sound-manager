f3e8461395f314cd178fcdbff47f66c8d4df8807
Alecell <agomes@newcore.com.br>
2020-08-11 02:32:11 -0300

    ci: finish cypress setup

---
+++ src/components/Header/tests/Header.spec.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { mount } from 'cypress-react-unit-test';
+import Header from '../Header';
+
+describe('HelloWorld component', () => {
+  it('works', () => {
+    expect(true).to.equal(true);
+  });
+});
