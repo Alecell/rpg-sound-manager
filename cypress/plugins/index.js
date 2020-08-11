4db170d5c1dc102bd988fd21d77a2d18c16e61cb
Alecell <agomes@newcore.com.br>
2020-08-11 00:13:48 -0300

    ci: initial cypress-react-unit-test config

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,5 +1,8 @@
-const cypressTypeScriptPreprocessor = require('./cy-ts-preprocessor')
+/* eslint-disable */
+const cypressTypeScriptPreprocessor = require('./cy-ts-preprocessor');
 
-module.exports = on => {
-  on('file:preprocessor', cypressTypeScriptPreprocessor)
-}
+module.exports = (on, config) => {
+  on('file:preprocessor', cypressTypeScriptPreprocessor);
+  require('cypress-react-unit-test/plugins/react-scripts')(on, config);
+  return config;
+};
