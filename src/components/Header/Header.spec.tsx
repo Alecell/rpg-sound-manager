91335795c0ad588bd1d1d87149721cb43195694d
Alecell <agomes@newcore.com.br>
2020-08-12 12:29:59 -0300

    refactor: move Header.spec outside test folder

--- src/components/Header/Header.spec.tsx
+++ src/components/Header/Header.spec.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { mount } from 'cypress-react-unit-test';
-import Header from '../Header';
+import Header from './Header';
 
 describe('HelloWorld component', () => {
   it('works', () => {
